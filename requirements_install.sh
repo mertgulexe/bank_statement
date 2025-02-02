3ca405a3a12ad5c2b517d404465625ebb85a2df8
mertgulexe <gul.mert89@gmail.com>
2025-02-02 21:43:15 +0000

    Add `--update` paramter

--- requirements_install.sh
+++ requirements_install.sh
@@ -3,13 +3,13 @@
 clear
 echo "[SYSTEM INFO] Installing the requirements..."
 sleep 2
-pip install git+https://github.com/huggingface/transformers --no-cache-dir
-pip install -r requirements.txt --no-cache-dir
+pip install git+https://github.com/huggingface/transformers --no-cache-dir --upgrade
+pip install -r requirements.txt --no-cache-dir --upgrade
 
 echo -e "\n[SYSTEM INFO] We also install \"flash attention\" for faster and memory-efficient inference."
 sleep 2
 echo "[SYSTEM INFO] Installing..."
-pip install flash-attn --no-build-isolation --no-cache-dir
+pip install flash-attn --no-build-isolation --no-cache-dir --upgrade
 
 echo -e "\n[SYSTEM INFO] Done.\n"
 sleep 0.5
